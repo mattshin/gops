e91a9077f1962b549bd3d1dbe4c145ea25d17c1c
Matthew <matthewshin2@gmail.com>
2023-04-11 17:58:19 -0500

    fix(client/random): process message as JSON

--- gops-player-templates/python/random_player/play.py
+++ gops-player-templates/python/random_player/play.py
@@ -1,17 +1,30 @@
+import json
 import os
 import random
+
 
 import rel
 import websocket
 
 
-def on_message(ws, data):
+def on_message(ws, message):
+    try:
+        data = json.loads(message)
+
+    except Exception as e:
+        print("did not receive json, ignoring")
+        print(repr(e))
+        return
+
     if data["info"]:
         print(data)
         return
 
     available_bids = data["my_bids"]
-    ws.send(random.choice(available_bids))
+    bid = random.choice(available_bids)
+
+    print(f"sending {bid=}")
+    ws.send(str(bid))
 
 
 def on_error(ws, error):
@@ -29,7 +42,7 @@ if __name__ == '__main__':
     gops_server = os.environ.get("GOPS_SERVER", "localhost:8000")
     game_id = os.environ.get("GAME_ID", "test_id")
 
-    ws = websocket.WebSocketApp(f"ws://{gops_server}/play/{game_id}",
+    ws = websocket.WebSocketApp(f"ws://{gops_server}/play/{game_id}?verbose=true",
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
